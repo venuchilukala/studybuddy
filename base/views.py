a80f7c91e38efaae51f4bb81de36d1a95cc8e167
venuchilukala <venuchilukala07@gmail.com>
2024-11-07 20:08:09 +0530

    CRUD operations

--- base/views.py
+++ base/views.py
@@ -1,13 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
+from .forms import RoomForm
 
 # Create your views here.
-
-# rooms = [
-#     {'id': 1, 'name': 'Learn Django'},
-#     {'id': 2, 'name': 'Techies'},
-#     {'id': 3, 'name': 'Python Developers'}
-# ]
 
 def home(request):
     rooms = Room.objects.all()
@@ -18,3 +13,35 @@ def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {'room' : room}
     return render(request, 'base/room.html', context)
+
+# ---- CRUD Operations ------ 
+# request.POST contains QueryDict like <QueryDict: {'csrfmiddlewaretoken': ['jt9k985UO5y13ym1z8Zy3oU6dvdaqZhj6BygNz1qfYyh9CaxP25PF4lMJDNwRtem'], 'host': ['1'], 'topic': ['3'], 'name': ['Data base'], 'description': ['']}>
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context ={'form': form}
+    return render(request, 'base/room_form.html', context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj':room})
