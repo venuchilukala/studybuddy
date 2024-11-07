776661c05548aa637b969d8fb5888a524dfc2d87
venuchilukala <venuchilukala07@gmail.com>
2024-11-07 20:58:14 +0530

    search added

--- base/views.py
+++ base/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from django.db.models import Q
+from .models import Room, Topic
 from .forms import RoomForm
 
 # Create your views here.
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains = q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+        )
+    rooms_count = rooms.count()
+
+    topics = Topic.objects.all()
+
+    context = {'rooms': rooms, 'topics': topics, 'rooms_count': rooms_count}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
