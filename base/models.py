9322539dc50f33569fb691dc14c253e835887573
venuchilukala <venuchilukala07@gmail.com>
2024-11-08 17:29:15 +0530

    user, login, delete pages

--- base/models.py
+++ base/models.py
@@ -13,7 +13,6 @@ class Room(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
-    # At the time of instance and while using form it may be null 
     description = models.TextField(null=True, blank=True)
     participants = models.ManyToManyField(User, related_name='participants', blank=True)
     updated = models.DateTimeField(auto_now=True)
